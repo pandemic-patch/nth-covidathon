5d07812a7c01788e833745d589214d24f0a8f174
Anton Lachmaniucu <antonmihail@gmail.com>
2020-05-25 21:17:26 -0400

    Updated Signup form

--- webapp/src/Patient/Tutorial/SignUp.js
+++ webapp/src/Patient/Tutorial/SignUp.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, Button, Checkbox } from 'semantic-ui-react';
+import { Form, Button, Checkbox, TextArea, Label } from 'semantic-ui-react';
 
 class SignUp extends Component {
   state = {
@@ -17,7 +17,9 @@ class SignUp extends Component {
     this.setState({ selectedReasons: updatedList });
   };
 
-  submitSignup = (evt) => {};
+  submitSignup = evt => {
+    //TODO Whatever we have on the backend for this should be triggered here
+  };
 
   render() {
     return (
@@ -29,8 +31,8 @@ class SignUp extends Component {
               pointing: 'below',
             }}
             fluid
-            label='Email'
-            placeholder='Email'
+            label='Email Address'
+            placeholder='Valid email address'
             icon='at'
           />
           <Form.Input
@@ -51,21 +53,104 @@ class SignUp extends Component {
           <Form.Field>What interested you about Pandemic Patch?</Form.Field>
           <Form.Field>
             <Checkbox
-              label='Choose this'
+              label='I can report an emergency at any time'
               name='1'
-              value='this'
-              checked={this.state.selectedReasons.find((el) => el.name === '1')}
+              value='I can report an emergency at any time'
+              checked={this.state.selectedReasons.find(el => el.name === '1')}
               onChange={this.handleChange}
             />
           </Form.Field>
           <Form.Field>
             <Checkbox
-              label='Or that'
+              label="I want to avoid Hospitals and Doctors' offices"
               name='2'
-              value='that'
-              checked={this.state.selectedReasons.find((el) => el.name === '2')}
+              value="I want to avoid Hospitals and Doctors' offices"
+              checked={this.state.selectedReasons.find(el => el.name === '2')}
               onChange={this.handleChange}
             />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox
+              label='I like that I can get help right at home'
+              name='3'
+              value='I like that I can get help right at home'
+              checked={this.state.selectedReasons.find(el => el.name === '3')}
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox
+              label='I want to visit my doctor online and pick up my prescriptions from the local drugstore'
+              name='4'
+              value='I want to visit my doctor online and pick up my prescriptions from the local drugstore'
+              checked={this.state.selectedReasons.find(el => el.name === '4')}
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox
+              label='I want to help solve COVID19'
+              name='5'
+              value='I want to help solve COVID19'
+              checked={this.state.selectedReasons.find(el => el.name === '5')}
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox
+              label='I think we need to be better prepared'
+              name='6'
+              value='I think we need to be better prepared'
+              checked={this.state.selectedReasons.find(el => el.name === '6')}
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox
+              label='I want to get the latest COVID19 news'
+              name='7'
+              value='I want to get the latest COVID19 news'
+              checked={this.state.selectedReasons.find(el => el.name === '7')}
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox
+              label='I would rather play a game than fill out boring medical forms'
+              name='8'
+              value='I would rather play a game than fill out boring medical forms'
+              checked={this.state.selectedReasons.find(el => el.name === '8')}
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox
+              label='I want to earn an income'
+              name='9'
+              value='I want to earn an income'
+              checked={this.state.selectedReasons.find(el => el.name === '9')}
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox
+              label='I want my family to be safe and stay healthy'
+              name='10'
+              value='I want my family to be safe and stay healthy'
+              checked={this.state.selectedReasons.find(el => el.name === '10')}
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Label>
+              Tell us something totally random about yourself. We want weird.
+              <br />
+              <sub>
+                I'll go first: I love putting vinegar and spicy mayonnaise on my
+                white rice. For breakfast.
+              </sub>
+            </Label>
+            <TextArea placeholder="Let's Hear It" />
           </Form.Field>
           <Button onClick={this.submitSignup} type='submit'>
             Submit
