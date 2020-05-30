1e1ffa06c212501ff1acbfc7e5d4e1e3a56196ec
Anton Lachmaniucu <antonmihail@gmail.com>
2020-05-30 18:50:52 -0400

    landing page done

--- webapp/src/Dashboard/Landing.js
+++ webapp/src/Dashboard/Landing.js
@@ -55,9 +55,9 @@ class Landing extends Component {
                     data-wow-delay='0.3s'
                     onClick={() => this.props.history.push('/signUp')}
                   >
-                    <a class='btn btn-default btn-home' href='#' role='button'>
+                    <button class='btn btn-default btn-home' role='button'>
                       Get Started
-                    </a>
+                    </button>
                   </div>
                 </div>
               </div>
@@ -79,55 +79,32 @@ class Landing extends Component {
             <div class='row'>
               <div class='col-md-7 col-sm-12 wow fadeInLeft'>
                 <div class='sub-heading'>
-                  <h3>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Magnam ipsa recusandae consequatur veniam, reiciendis odit
-                    quia eaque vel eius a.
-                  </h3>
+                  <h3>Join the Zeemz Army & fight Coronavirus.</h3>
                 </div>
                 <div class='block'>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ulla-mco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in
+                    Personal privacy and data ownership are at the heart of our
+                    design. Pandemic Patch will not send your data without
+                    permission. If you grant permission, we'll only send your
+                    anonymized data to the central database run by the crew
+                    behind <a href='https://www.askco19.com/'>askco19.com</a>,
+                    they're a group of doctors inputting real time data from
+                    their front line experiences with CoVID-19. They're asking
+                    for help collecting data to stop the spread.
                   </p>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Neque, aspernatur.
-                  </p>
+                  <p>Zeemz: Pandemic Patch is our answer to their call.</p>
                 </div>
               </div>
               <div
                 class='col-md-5 col-sm-12 wow fadeInLeft'
                 data-wow-delay='0.3s'
               >
-                <div class='about-slider'>
-                  <div class='init-slider'>
-                    <div class='about-item'>
-                      <img
-                        src='images/about/1.jpg'
-                        alt=''
-                        class='img-responsive'
-                      />
-                    </div>
-                    <div class='about-item'>
-                      <img
-                        src='images/about/2.jpg'
-                        alt=''
-                        class='img-responsive'
-                      />
-                    </div>
-                    <div class='about-item'>
-                      <img
-                        src='images/about/3.jpg'
-                        alt=''
-                        class='img-responsive'
-                      />
-                    </div>
-                  </div>
-                </div>
+                <img
+                  src='https://lh5.googleusercontent.com/yXDwcAZPnV8Sb0oXo_7Ek7-_KY-i9kcXvqfzHOjcjd9fI4WWvfHMpV9y1PhhUmwfQTqCTn6H=w16383'
+                  alt=''
+                  height='100%'
+                  width='100%'
+                />
               </div>
             </div>
           </div>
@@ -137,27 +114,26 @@ class Landing extends Component {
           <div class='container'>
             <div class='row'>
               <div class='heading wow fadeInUp'>
-                <h2>Our service</h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et <br /> dolore magna
-                  aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                  ullamco laboris nisi ut aliquip ex
-                </p>
+                <h2>Donations</h2>
+                <p></p>
               </div>
               <div class='col-sm-6 col-md-3 wow fadeInLeft'>
                 <div class='service'>
                   <div class='icon-box'>
-                    <span class='icon'>
-                      <i class='ion-android-desktop'></i>
-                    </span>
+                    <img
+                      src='https://lh4.googleusercontent.com/NYpUxJNplOuoinI3M5mnqmzUHENGtAHvQY7WhAwF8BRhU-swOjNP4Mp2cxX5LVwLjAqA-RApu3-9wo45Jf75MhpomAXZO_XpPMycI4zQ9GvFQDYA3d0=w1280'
+                      alt='fiat'
+                      width='100%'
+                      height='100%'
+                    />
                   </div>
                   <div class='caption'>
-                    <h3>Fully Responsive</h3>
-                    <p>
-                      Lorem ipsum dolor sit amet, con-sectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut
-                    </p>
+                    <a href='https://www.zeemz.xyz/donate-fiat'>
+                      <u>
+                        <h3>Fiat</h3>
+                      </u>
+                    </a>
+                    <p>USD, EUR, more...</p>
                   </div>
                 </div>
               </div>
@@ -167,16 +143,20 @@ class Landing extends Component {
               >
                 <div class='service'>
                   <div class='icon-box'>
-                    <span class='icon'>
-                      <i class='ion-speedometer'></i>
-                    </span>
+                    <img
+                      src='https://lh6.googleusercontent.com/auvAl5Gefo9jXcnBCGKETEgmkodBF_APNWivFbu8pjxaZ_2MTSfhWdLF6taDepmIQYDhDfCl3Ysz9EkeCzcprH-nFtEcLYfZP7ol1brvA61XhmjnKg=w1280'
+                      alt='fiat'
+                      width='100%'
+                      height='100%'
+                    />
                   </div>
                   <div class='caption'>
-                    <h3>Speed Optimized</h3>
-                    <p>
-                      Lorem ipsum dolor sit amet, con-sectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut
-                    </p>
+                    <a href='https://www.zeemz.xyz/donate-crypto'>
+                      <u>
+                        <h3>Crypto</h3>
+                      </u>
+                    </a>
+                    <p>BTC, ETH, more...</p>
                   </div>
                 </div>
               </div>
@@ -186,16 +166,20 @@ class Landing extends Component {
               >
                 <div class='service'>
                   <div class='icon-box'>
-                    <span class='icon'>
-                      <i class='ion-ios-infinite-outline'></i>
-                    </span>
+                    <img
+                      src='https://lh5.googleusercontent.com/h7DS7wwnWoYT8xN5pwc8BF8YdB5gjvDK_czLxRN539tAAqrI1imSeq48FMJ94F4xiAjFKMQllnV-MtkNAVswtzJxRNuXa5zQ_Uq95tMgaFL6011_BQ=w1280'
+                      alt='fiat'
+                      width='100%'
+                      height='100%'
+                    />
                   </div>
                   <div class='caption'>
-                    <h3>Tons of Feature</h3>
-                    <p>
-                      Lorem ipsum dolor sit amet, con-sectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut
-                    </p>
+                    <a href='https://www.zeemz.xyz/donate-skills'>
+                      <u>
+                        <h3>Desgin / Coding</h3>
+                      </u>
+                    </a>
+                    <p>Have some great ideas? We love to hear about them!</p>
                   </div>
                 </div>
               </div>
@@ -205,16 +189,20 @@ class Landing extends Component {
               >
                 <div class='service'>
                   <div class='icon-box'>
-                    <span class='icon'>
-                      <i class='ion-ios-cloud-outline'></i>
-                    </span>
+                    <img
+                      src='https://lh4.googleusercontent.com/Pl7wXHxtgQn6GT0lvQYDJkG-09nFysK7yINW2wWbfzv95lZJlWeVV28d22CAOdR6o6h1g_cxjd3b7y6YyTJ2ps1j0kXsET1PYZaZqqT22-i3QEf26Ew=w1280'
+                      alt='fiat'
+                      width='100%'
+                      height='100%'
+                    />
                   </div>
                   <div class='caption'>
-                    <h3>Cloud Option</h3>
-                    <p>
-                      Lorem ipsum dolor sit amet, con-sectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut
-                    </p>
+                    <a href='https://www.zeemz.xyz/donate-social-media'>
+                      <u>
+                        <h3>Social Media</h3>
+                      </u>
+                    </a>
+                    <p>Want to help out and earn extra coins?</p>
                   </div>
                 </div>
               </div>
@@ -227,10 +215,16 @@ class Landing extends Component {
             <div class='row'>
               <div class='col-md-12 wow text-center'>
                 <div class='block'>
-                  <h2>Lorem ipsum dolor sit amet, consectetur adipisicing</h2>
+                  <h2>
+                    Sign up for alpha testing! and be one of the first to play,
+                    create, and collect Zeemz
+                  </h2>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod
+                    We´ll send you an email when the game is up and ready to
+                    play. <br /> Our pre-alpha testers will have access to VIP
+                    forums, early release equipment and upgrades, and
+                    development team emails that will keep you up-to-date on the
+                    latest additions!
                   </p>
                   <div class='form-group'>
                     <input
@@ -247,92 +241,42 @@ class Landing extends Component {
             </div>
           </div>
         </section>
-
+        <hr />
         <section id='contact' class='section'>
           <div class='container'>
             <div class='row'>
-              <div class='col-xs-12 col-sm-12 col-md-12'>
-                <div class='block'>
-                  <div class='heading wow fadeInUp'>
-                    <h2>Get In Touch</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut labore et <br />{' '}
-                      dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                      exercitation ullamco laboris nisi ut aliquip ex
-                    </p>
-                  </div>
-                </div>
-              </div>
               <div class='col-xs-12 col-sm-12 col-md-5 wow fadeInUp'>
                 <div class='block text-left'>
                   <div class='sub-heading'>
-                    <h4>Contact Address</h4>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                      Amet quos maxime voluptate doloribus, voluptatum nemo vel
-                      ipsa in eligendi, ullam. Ducimus consequuntur labore error
-                      hic.
-                    </p>
+                    <h4>Useful Links</h4>
                   </div>
                   <address class='address'>
                     <hr />
                     <p>
-                      Dhanmondi 32,
-                      <br /> Dhaka,
-                      <br /> Bangladesh
+                      <b>
+                        <u>
+                          <a href='https://github.com/pushingbhutons/zeemz-ann'>
+                            Github [ANN]
+                          </a>
+                          <br />
+                          <a href='https://www.pushingbhutons.com/'>
+                            Pu'Shing Bhu'Tons
+                          </a>
+                          <br />
+                          <a href='https://www.zazzle.com/store/pushing_bhutons'>
+                            Pu'Shing Bhu'Tons Gear
+                          </a>
+                        </u>
+                      </b>
                     </p>
                     <hr />
                     <p>
-                      <strong>E:</strong>&nbsp;hello@foundry.net
-                      <br />
-                      <strong>P:</strong>&nbsp;+614 3948 2726
+                      ETH Donations:{' '}
+                      <a href='https://etherscan.io/address/0x54dd3364be1816a7717d60ac6ef3a0e1b06bf167'>
+                        0x54dd3364BE1816A7717D60ac6EF3a0E1B06Bf167
+                      </a>
                     </p>
                   </address>
-                </div>
-              </div>
-
-              <div
-                class='col-xs-12 col-sm-12 col-md-5 col-md-offset-1 wow fadeInUp'
-                data-wow-delay='0.3s'
-              >
-                <div class='form-group'>
-                  <form action='#' method='post' id='contact-form'>
-                    <div class='input-field'>
-                      <input
-                        type='text'
-                        class='form-control'
-                        placeholder='Your Name'
-                        name='name'
-                      />
-                    </div>
-                    <div class='input-field'>
-                      <input
-                        type='email'
-                        class='form-control'
-                        placeholder='Email Address'
-                        name='email'
-                      />
-                    </div>
-                    <div class='input-field'>
-                      <textarea
-                        class='form-control'
-                        placeholder='Your Message'
-                        rows='3'
-                        name='message'
-                      ></textarea>
-                    </div>
-                    <button class='btn btn-send' type='submit'>
-                      Send me
-                    </button>
-                  </form>
-
-                  <div id='success'>
-                    <p>Your Message was sent successfully</p>
-                  </div>
-                  <div id='error'>
-                    <p>Your Message was not sent successfully</p>
-                  </div>
                 </div>
               </div>
             </div>
@@ -349,9 +293,18 @@ class Landing extends Component {
               <div class='col-md-12'>
                 <div class='block'>
                   <p>
-                    Copyright &copy;{' '}
-                    <a href='http://www.Themefisher.com'>Themefisher</a>| All
-                    right reserved.
+                    <b>Copyright © 2020 ZEEMZ v. 0.2.0-preAlpha</b>
+                    <br />
+                    is part of the
+                    <br />
+                    <b>Copyright © 2020 Pu´Shing Bhu´Tons Universe</b>
+                    <br />
+                    created by
+                    <br />
+                    Monique Finley and Jennifer Landry. All Rights Reserved.
+                    Website developed by{' '}
+                    <a href='https://www.pir8aye.net/'>Pir8eye</a>. Last update:
+                    Feb 2020.
                   </p>
                 </div>
               </div>
